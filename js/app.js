ef0eb10755b7151b93de17ea830cf9fe4280a074
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2021-12-12 11:41:57 +0100

    add class date

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 const divDay = document.createElement("div");
+const d = new Date();
+
 const decompte= new Decompte(2021,12,25,24,0,59, divDay);
-const date = document.createElement("div");
 
 document.body.appendChild(divDay);
-document.body.appendChild(date);
+
 
 divDay.style.backgroundColor = "pink";
 divDay.style.width = "25rem";
